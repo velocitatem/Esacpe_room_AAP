8f0cb492d7ddcb31830d11f254c2e1defd863609
Daniel Rosel <danalves24@outlook.com>
2020-02-02 13:51:50 +0100

    header-dove

---
+++ aap-er/src/Components/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Head = styled.div`
+text-align: center;
+`
+const Title = styled.h1``
+function header() {
+    return (
+        <Head>
+            <Title>AAP Escape Room</Title>
+        </Head>
+    )
+
+}
+export default header;
--- aap-er/src/index.js
+++ aap-er/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import styled from 'styled-components';
 //importing elements
-
+import Header from './Components/Header/header';
 
 //stylying pages
 const AAP = styled.div`
 background-color: black;
+font-family: 'Sigmar One', cursive;
+color: #8a0303;
 `
 ReactDOM.render(
     <AAP>
-
+        <Header />
     </AAP> ,
     
     document.getElementById('root'));
