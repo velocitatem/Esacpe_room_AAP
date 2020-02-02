14327f2ed00a79310d7c24c897f63107912251bb
Daniel Rosel <danalves24@outlook.com>
2020-02-02 13:43:57 +0100

    env-edit

--- aap-er/src/index.js
+++ aap-er/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import styled from 'styled-components';
+//importing elements
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//stylying pages
+const AAP = styled.div`
+background-color: black;
+`
+ReactDOM.render(
+    <AAP>
+
+    </AAP> ,
+    
+    document.getElementById('root'));
+
 serviceWorker.unregister();
